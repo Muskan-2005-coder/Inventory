89f1369c67a97d66027efa32adad97e25c7c7d25
Pranav <lifeispranav@gmail.com>
2025-06-18 14:52:02 +0530

    Feat: Exported models

---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  Alerts: require('./alert.model'),
+  Deliveries: require('./delivery.model'),
+  Inventory: require('./inventory.model'),
+  Users: require('./user.model'),
+  Wages: require('./wages.model')
+}
