743c9c83a53eec498778f292019310f3bcd5b32e
Pranav <lifeispranav@gmail.com>
2025-06-18 14:44:17 +0530

    Feat: Added wagesmodel

---
+++ src/models/wages.model.js
@@ -0,0 +1,53 @@
+/**
+ * Wages/Payroll Model Fields
+ *
+ * userId        : ObjectId   // Reference to user
+ * month         : Date       // Month of payroll
+ * hoursWorked   : number     // Calculated from shifts
+ * totalSalary   : number     // Computed from wage/hour
+ * overworked    : boolean    // True if exceeded healthy hours
+ * calculatedAt  : Date       // Payroll run date
+ * createdAt     : Date       // Timestamp
+ * updatedAt     : Date       // Timestamp
+ */
+
+const mongoose = require('mongoose');
+
+const wageSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+
+  month: {
+    type: Date,
+    default: Date.now()
+  },
+
+  hoursWorked: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+
+  totalSalary: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+
+  overworked: {
+    type: Boolean,
+    default: false
+  },
+
+  calculatedAt: {
+    type: Date,
+    default: Date.now
+  }
+
+}, { timestamps: true });
+
+const WageModel = mongoose.model('wage', wageSchema);
+module.exports = WageModel;
