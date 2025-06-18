2ac9f92d44d00973b05e2ed49aeccea35e8a3839
Pranav <lifeispranav@gmail.com>
2025-06-18 14:20:37 +0530

    Feat: Added inventory model

---
+++ src/models/inventory.model.js
@@ -0,0 +1,118 @@
+/**
+ * Product Model Fields
+ *
+ * productName         : string     // Name of the product
+ * category            : string     // E.g., “grains”, “electronics”
+ * quantity            : number     // Available units
+ * location            : string     // Warehouse location (Place to store it/Rack)
+ * supplierId          : ObjectId   // Link to supplier (user)
+ * expiryDate          : Date       // For perishable items
+ * thresholdLimit      : number     // When to trigger low stock alert
+ * shelfLifeDays       : number     // Expected life span
+ * restockRecommended  : boolean    // Used by AI to suggest restocking
+ * createdAt           : Date       // Timestamp
+ * updatedAt           : Date       // Timestamp
+ */
+
+const productCategories = [
+  "groceries",
+  "freshProduce",
+  "dairyEggs",
+  "meatSeafood",
+  "frozenFoods",
+  "beverages",
+  "snacks",
+  "bakery",
+  "pantryEssentials",
+  "cleaningHousehold",
+  "healthWellness",
+  "personalCare",
+  "babyProducts",
+  "petSupplies",
+  "electronics",
+  "computersAccessories",
+  "homeKitchen",
+  "furniture",
+  "appliances",
+  "toolsHardware",
+  "automotive",
+  "sportsOutdoors",
+  "toysGames",
+  "booksMedia",
+  "stationeryOffice",
+  "clothingMen",
+  "clothingWomen",
+  "clothingKids",
+  "footwear",
+  "jewelryAccessories",
+  "beautyCosmetics"
+];
+
+const storageLocations = [
+  "A1",
+  "A2",
+  "A3",
+  "B1",
+  "B2",
+  "B3",
+  "C1",
+  "C2",
+  "C3",
+  "D1",
+  "D2",
+  "FzO1",
+  "FzO2",
+  "notAllotted"
+];
+
+const mongoose = require("mongoose");
+
+const inventorySchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true
+  },
+
+  category: {
+    type: String,
+    enum: productCategories
+  },
+
+  quantity: {
+    type: Number,
+    default: 0
+  },
+
+  location: {
+    type: String,
+    enum: storageLocations,
+    default: 'notAllotted'
+  },
+
+  supplierId: {
+    type: String,
+    required: true
+  },
+
+  expiryDate: {
+    type: Date
+  },
+
+  thresholdLimit: {
+    type: number
+  },
+
+  shelfLifeDays: {
+    type: Number,
+    default: 0
+  },
+
+  restockRecommended: {
+    type: Boolean,
+    default: false
+  },
+
+}, { timestamps: true })
+
+const inventoryModel = mongoose.model('inventory', inventorySchema)
+module.exports = inventoryModel
