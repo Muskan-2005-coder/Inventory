562b665b80a15fca0124311f3a16c7008877a05b
Pranav <lifeispranav@gmail.com>
2025-06-18 20:45:51 +0530

    Feat: Add user Dto for updating user

--- src/dto/user.dto.js
+++ src/dto/user.dto.js
@@ -1,16 +1,21 @@
 const { z } = require("zod")
 
 const createUserSchema = z.object({
-  name: z.string().min(3),
-  username: z.string().min(3).max(10).transform(val => val.toLowerCase()),
+  name: z.string().min(3).max(20),
   email: z.string().email().transform(val => val.toLowerCase()),
   password: z.string().min(4).max(10),
-  role: z.string().optional(),
-  phone: z.optional()
+  role: z.string().enum(['admin', 'staff', 'supplier', 'driver'])
 }).strict()
 
 const updateUserSchema = z.object({
-
+  name: z.string().min(3).max(20).optional(),
+  username: z.string().min(3).max(10).optional(),
+  email: z.string().email().transform(val => val.toLowerCase()).optional(),
+  role: z.enum(['admin', 'staff', 'supplier', 'driver']).optional(),
+  phone: z.string().length(10).optional(),
+  shift: z.enum(['morning', 'evening', 'night']).optional(),
+  wagePerHour: z.coerce.number().optional(),
+  active: z.boolean().optional().default(true)
 })
 
 module.exports = {
