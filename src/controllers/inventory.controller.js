6a5123c436b292264ed29d738f852febbde24ec6
Pranav <lifeispranav@gmail.com>
2025-06-19 20:42:01 +0530

    Feat: Added Inventory Controller

--- src/controllers/inventory.controller.js
+++ src/controllers/inventory.controller.js
@@ -1,43 +1,76 @@
-const { StatusCodes } = require("http-status-codes")
+const { StatusCodes } = require('http-status-codes');
+const { InventoryService } = require('../services');
+const { InventoryRepository, ProductRepository } = require('../repositories');
+const logger = require('../utils/logger');
+const ApiResponse = require('../dto/response.dto');
 
-const getAllInventory = async(req, res) => {
-  res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented Yet "})
-}
+const CONTEXT = 'InventoryController';
 
-const addNewItem = async(req, res) => {
-  res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented Yet "})
-}
+const inventoryService = new InventoryService(new InventoryRepository(), new ProductRepository());
 
-const getItem = async(req, res) => {
-  res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented Yet "})
-}
-const updateItem = async(req, res) => {
-  res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented Yet "})
-}
+const createInventory = async (req, res) => {
+  logger.info(`[${CONTEXT}] Creating new inventory with data: ${JSON.stringify(req.body)}`);
+  const inventory = await inventoryService.createInventory(req.body);
+  res.status(StatusCodes.CREATED).json(ApiResponse.success('Inventory created successfully', { inventory }));
+  logger.info(`[${CONTEXT}] Inventory created successfully: ${inventory._id}`);
+};
 
-const deleteItem = async(req, res) => {
-  res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented Yet "})
-}
+const getInventory = async (req, res) => {
+  logger.info(`[${CONTEXT}] Fetching inventory with ID: ${req.params.id}`);
+  const inventory = await inventoryService.getInventory(req.params.id);
+  res.status(StatusCodes.OK).json(ApiResponse.success('Inventory fetched successfully', { inventory }));
+  logger.info(`[${CONTEXT}] Inventory fetched successfully: ${inventory._id}`);
+};
 
-const getLowStockItems = async(req, res) => {
-  res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented Yet "})
-}
+const getAllInventories = async (req, res) => {
+  logger.info(`[${CONTEXT}] Fetching all inventories`);
+  const inventories = await inventoryService.getAllInventories();
+  res.status(StatusCodes.OK).json(ApiResponse.success('All inventories fetched successfully', { length: inventories.length, inventories }));
+  logger.info(`[${CONTEXT}] All inventories fetched successfully. Total: ${inventories.length}`);
+};
 
-const getRestockSuggestion = async(req, res) => {
-  res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented Yet "})
-}
+const updateInventory = async (req, res) => {
+  logger.info(`[${CONTEXT}] Updating inventory ID: ${req.params.id} with data: ${JSON.stringify(req.body)}`);
+  const updatedInventory = await inventoryService.updateInventory(req.params.id, req.body);
+  res.status(StatusCodes.OK).json(ApiResponse.success('Inventory updated successfully', { inventory: updatedInventory }));
+  logger.info(`[${CONTEXT}] Inventory updated successfully: ${updatedInventory._id}`);
+};
 
-const getHeatmap = async(req, res) => {
-  res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented Yet "})
-}
+const deleteInventory = async (req, res) => {
+  logger.info(`[${CONTEXT}] Deleting inventory ID: ${req.params.id}`);
+  const deletedInventory = await inventoryService.deleteInventory(req.params.id);
+  res.status(StatusCodes.OK).json(ApiResponse.success('Inventory deleted successfully', { inventory: deletedInventory }));
+  logger.info(`[${CONTEXT}] Inventory deleted successfully: ${deletedInventory._id}`);
+};
+
+const addProductToInventory = async (req, res) => {
+    logger.info(`[${CONTEXT}] Adding product ${req.body.productId} to inventory ${req.params.id}`);
+    const inventory = await inventoryService.addProductToInventory(req.params.id, req.body.productId);
+    res.status(StatusCodes.OK).json(ApiResponse.success('Product added to inventory successfully', { inventory }));
+    logger.info(`[${CONTEXT}] Product added to inventory successfully.`);
+};
+
+const removeProductFromInventory = async (req, res) => {
+    logger.info(`[${CONTEXT}] Removing product ${req.body.productId} from inventory ${req.params.id}`);
+    const inventory = await inventoryService.removeProductFromInventory(req.params.id, req.body.productId);
+    res.status(StatusCodes.OK).json(ApiResponse.success('Product removed from inventory successfully', { inventory }));
+    logger.info(`[${CONTEXT}] Product removed from inventory successfully.`);
+};
+
+const getInventoryCapacityUtilization = async (req, res) => {
+    logger.info(`[${CONTEXT}] Getting capacity utilization for inventory ${req.params.id}`);
+    const utilization = await inventoryService.getInventoryCapacityUtilization(req.params.id);
+    res.status(StatusCodes.OK).json(ApiResponse.success('Inventory capacity utilization fetched successfully', { utilization }));
+    logger.info(`[${CONTEXT}] Inventory capacity utilization fetched successfully.`);
+};
 
 module.exports = {
-  getAllInventory,
-  addNewItem,
-  getItem,
-  updateItem,
-  deleteItem,
-  getLowStockItems,
-  getRestockSuggestion,
-  getHeatmap
-}
+  createInventory,
+  getInventory,
+  getAllInventories,
+  updateInventory,
+  deleteInventory,
+  addProductToInventory,
+  removeProductFromInventory,
+  getInventoryCapacityUtilization
+};
