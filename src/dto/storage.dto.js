5b014e110a66fa9103f7812c3431293085c11fc0
Pranav <lifeispranav@gmail.com>
2025-06-20 16:05:54 +0530

    feat: storage dto made

--- src/dto/storage.dto.js
+++ src/dto/storage.dto.js
@@ -3,18 +3,18 @@ const { z } = require('zod')
 const createStorageSchema = z.object({
 
   locationId: z.string().min(2).max(40),
-  dimensions: z.object({ length: z.number(), width: z.number(), height: z.number()}).optional(),
+  dimensions: z.object({ length: z.number(), width: z.number(), height: z.number()}),
   holdingCapacity: z.number(),
-  volume: z.number().optional()
+  Volume: z.number().optional()
 
 }).strict()
 
 const updateStorageSchema = z.object({
 
-  locationId: z.string().min(2).max(40),
+  locationId: z.string().min(2).max(40).optional(),
   dimensions: z.object({ length: z.number(), width: z.number(), height: z.number()}).optional(),
-  holdingCapacity: z.number(),
-  capacityOccupied: z.number(),
+  holdingCapacity: z.number().optional(),
+  capacityOccupied: z.number().optional(),
   Volume: z.number().optional(),
   VolumeOccupied: z.number().optional(),
   products: z.array(z.string()).optional(),
@@ -22,11 +22,11 @@ const updateStorageSchema = z.object({
 }).strict()
 
 const addProductSchema = z.object({
-  products: z.string()
+  productId: z.string()
 }).strict()
 
 const removeProductSchema = z.object({
-  products: z.string()
+  productId: z.string()
 }).strict()
 
 module.exports = {
